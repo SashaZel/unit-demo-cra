282c1452538cb140d2910094854b10fd1962a780
Alexander Zelenkov <lll555@yandex.ru>
2023-07-16 00:40:16 +0700

    fix: onTag yml oktocat

--- ci/createIssue.js
+++ ci/createIssue.js
@@ -8,19 +8,24 @@ const octokit = new Octokit({
   auth: GH_TOKEN
 })
 
-await octokit.request('POST /repos/SashaZel/unit-demo-cra/issues', {
-  owner: 'SashaZel',
-  repo: 'unit-demo-cra',
-  title: 'Found a bug',
-  body: 'I\'m having a problem with this.',
-  assignees: [
-    'octocat'
-  ],
-  milestone: 1,
-  labels: [
-    'bug'
-  ],
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-})
+async function createIssue() {
+
+  await octokit.request('POST /repos/SashaZel/unit-demo-cra/issues', {
+    owner: 'SashaZel',
+    repo: 'unit-demo-cra',
+    title: 'Found a bug',
+    body: 'I\'m having a problem with this.',
+    assignees: [
+      'octocat'
+    ],
+    milestone: 1,
+    labels: [
+      'bug'
+    ],
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+createIssue();
