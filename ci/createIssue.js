0fac83791ce2ae623aed0788485db64b8f0db8a4
Alexander Zelenkov <lll555@yandex.ru>
2023-07-16 16:12:55 +0700

    chore: clean up code

--- ci/createIssue.js
+++ ci/createIssue.js
@@ -1,21 +1,16 @@
-// Octokit.js
-// https://github.com/octokit/core.js#readme
 const { Octokit } = require("@octokit/core");
-const { readFileSync } = require("fs")
+const { readFileSync } = require("fs");
 
 const GH_TOKEN = process.env.GH_TOKEN;
-const GH_REPO = process.env.GH_REPO;
-const GH_ACTOR = process.env.GH_ACTOR;
 const GH_REF_NAME = process.env.GH_REF_NAME;
-const GH_SHA = process.env.GH_SHA;
-const GITHUB_CONTEXT = process.env.GITHUB_CONTEXT;
 
-//console.log("@github context ", GITHUB_CONTEXT);
+// const GITHUB_CONTEXT = process.env.GITHUB_CONTEXT;
+// console.log("@github context ", GITHUB_CONTEXT);
 
-let report 
+let report;
 try {
-report = readFileSync("./ci/report.txt", "utf-8");
-} catch(error) {
+  report = readFileSync("./ci/report.txt", "utf-8");
+} catch (error) {
   console.error("@createIssue Error: no report file found");
 }
 
@@ -24,36 +19,22 @@ const octokit = new Octokit({
 });
 
 async function main() {
-  // let tagData;
-  // try {
-  //   tagData = await octokit.request(`GET /repos/SashaZel/unit-demo-cra/git/tags/${GH_SHA}`, {
-  //     owner: "SashaZel",
-  //     repo: "unit-demo-cra",
-  //     tag_sha: GH_SHA,
-  //     headers: {
-  //       "X-GitHub-Api-Version": "2022-11-28",
-  //     },
-  //   });
-  // } catch (error) {
-  //   console.error("@createIssue.js Error: fail to get tag data ", error);
-  //   process.exit(1);
-  // }
-
-  // Get SHA for current tag
   let tagSHAData;
   try {
-    tagSHAData = await octokit.request(`GET /repos/SashaZel/unit-demo-cra/git/refs/tags/${GH_REF_NAME}`, {
-      owner: "SashaZel",
-      repo: "unit-demo-cra",
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    });
+    tagSHAData = await octokit.request(
+      `GET /repos/SashaZel/unit-demo-cra/git/refs/tags/${GH_REF_NAME}`,
+      {
+        owner: "SashaZel",
+        repo: "unit-demo-cra",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
   } catch (error) {
     console.error("@createIssue.js Error: fail to get tag sha ", error);
     process.exit(1);
   }
-  //console.log("tagSHAdata ", tagSHAData)
   const currentTagSHA = tagSHAData.data.object.sha;
 
   // Get current tag info
@@ -70,25 +51,7 @@ async function main() {
     console.error("@createIssue.js Error: fail to get tag sha ", error);
     process.exit(1);
   }
-  //console.log("tagInfo ", tagInfo);
-  const tagInfoFormatted = `**Tag author:** ${tagInfo.data.tagger.name}\n**Tag date:** ${tagInfo.data.tagger.date}\n**Tag message:** ${tagInfo.data.message}\n \n`
-
-  // Can get all tags
-  // let tagData;
-  // try {
-  //   tagData = await octokit.request("GET /repos/SashaZel/unit-demo-cra/tags", {
-  //     owner: "SashaZel",
-  //     repo: "unit-demo-cra",
-  //     headers: {
-  //       "X-GitHub-Api-Version": "2022-11-28",
-  //     },
-  //   });
-  // } catch (error) {
-  //   console.error("@createIssue.js Error: fail to get tags data ", error);
-  //   process.exit(1);
-  // }
-
-  //console.log("tagsData ", JSON.stringify(tagData));
+  const tagInfoFormatted = `**Tag author:** ${tagInfo.data.tagger.name}\n**Tag date:** ${tagInfo.data.tagger.date}\n**Tag message:** ${tagInfo.data.message}\n \n`;
 
   let compareData;
   let previousTagNumber = Number(GH_REF_NAME.slice(1) - 1);
@@ -117,15 +80,11 @@ async function main() {
     const commitMsg = `- ${current.commit.committer.name} ${current.commit.committer.date}\n    "${current.commit.message}"\n    SHA: ${current.sha}\n \n`;
     changelogFormatted += commitMsg;
   }
-  // const issueBody = `Release ${tagData.properties.tag.examples[0]}
-  // Created by: ${tagData.properties.tagger.properties.name.type}
-  // ${tagData.properties.date.type}
-  // ${GH_REPO}`
 
   const issueBody = `# Release ${GH_REF_NAME}\n ${tagInfoFormatted}\n## Changelog between v${previousTagNumber} and ${GH_REF_NAME}: \n \n ${changelogFormatted} \n
   ## Tests report\n \n${report}`;
 
-  let issueCreateResult
+  let issueCreateResult;
   try {
     issueCreateResult = await octokit.request("POST /repos/SashaZel/unit-demo-cra/issues", {
       owner: "SashaZel",
